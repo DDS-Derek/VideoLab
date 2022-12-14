c6e040e1834dbac05c6a4f8b99d2cc2529abcfed
DDSRem <1448139087@qq.com>
2022-12-14 23:04:35 +0800

    优化日志输出

--- scripts/File_Permission_Changes_Plus.sh
+++ scripts/File_Permission_Changes_Plus.sh
@@ -11,21 +11,24 @@ Font="\033[0m"
 INFO="[${Green}INFO${Font}]"
 ERROR="[${Red}ERROR${Font}]"
 WARN="[${Blue}WARN${Font}]"
+Time=$(date +"%Y-%m-%d %T")
 
 check_logs(){
 if [ ! -d ./logs ]; then
     mkdir ./logs
 fi
+if [ ! -f ./logs/main.log ]; then
+    touch ./logs/main.log
+fi
 }
 
 check_dir(){
 # chown dir
-
 find ${Media_DIR} \
     -type d \
     ! -group ${PGID} \
     -or \
-    ! -user ${PUID} >> ./logs/dir_changes_list.log
+    ! -user ${PUID} > ./logs/dir_changes_list.log
 
 find ${Media_DIR} \
     -type d \
@@ -33,6 +36,14 @@ find ${Media_DIR} \
     -or \
     ! -user ${PUID} \
     -exec chown ${PUID}:${PGID} {} \;
+if [ $? -eq 0 ]; then
+    echo -e "${INFO} 文件夹所属组设置成功"
+    echo -e "${Time} INFO 文件夹所属组设置成功" >> ./logs/main.log
+else
+    echo -e "${ERROR} 文件夹所属组设置失败"
+    echo -e "${Time} ERROR 文件夹所属组设置失败" >> ./logs/main.log
+    exit 1
+fi
 
 # chmod dir
 find ${Media_DIR} \
@@ -43,6 +54,14 @@ find ${Media_DIR} \
     -type d \
     ! -perm ${CFVR} \
     -exec chmod ${CFVR} {} \;
+if [ $? -eq 0 ]; then
+    echo -e "${INFO} 文件夹访问权限设置成功"
+    echo -e "${Time} INFO 文件夹访问权限设置成功" >> ./logs/main.log
+else
+    echo -e "${ERROR} 文件夹访问权限设置失败"
+    echo -e "${Time} ERROR 文件夹访问权限设置失败" >> ./logs/main.log
+    exit 1
+fi
 }
 
 check_file(){
@@ -51,7 +70,7 @@ find ${Media_DIR} \
     -type f \
     ! -group ${PGID} \
     -or \
-    ! -user ${PUID} >> ./logs/file_changes_list.log
+    ! -user ${PUID} > ./logs/file_changes_list.log
 
 find ${Media_DIR} \
     -type f \
@@ -59,6 +78,14 @@ find ${Media_DIR} \
     -or \
     ! -user ${PUID} \
     -exec chown ${PUID}:${PGID} {} \;
+if [ $? -eq 0 ]; then
+    echo -e "${INFO} 文件所属组设置成功"
+    echo -e "${Time} INFO 文件所属组设置成功" >> ./logs/main.log
+else
+    echo -e "${ERROR} 文件所属组设置失败"
+    echo -e "${Time} ERROR 文件所属组设置失败" >> ./logs/main.log
+    exit 1
+fi
 
 # chmod file
 find ${Media_DIR} \
@@ -69,6 +96,14 @@ find ${Media_DIR} \
     -type f \
     ! -perm ${CFVR} \
     -exec chmod ${CFVR} {} \;
+if [ $? -eq 0 ]; then
+    echo -e "${INFO} 文件访问权限设置成功"
+    echo -e "${Time} INFO 文件访问权限设置成功" >> ./logs/main.log
+else
+    echo -e "${ERROR} 文件访问权限设置失败"
+    echo -e "${Time} ERROR 文件访问权限设置失败" >> ./logs/main.log
+    exit 1
+fi
 }
 
 check_change(){
