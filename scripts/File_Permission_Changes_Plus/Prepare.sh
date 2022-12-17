19417b20a90622ba175e333ec780b34326e80d24
DDSRem <1448139087@qq.com>
2022-12-17 15:50:39 +0800

    fix

--- scripts/File_Permission_Changes_Plus/Prepare.sh
+++ scripts/File_Permission_Changes_Plus/Prepare.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+. /etc/videolab/settings.sh
+. ${config_dir}/nastools_all_in_one/basic_settings.sh
+
 Blue="\033[34m"
 Green="\033[32m"
 Red="\033[31m"
@@ -28,15 +31,41 @@ else
     TEXT='Crontab 已安装' && INFO
 fi
 
-. /etc/videolab/settings.sh
-. ${config_dir}/nastools_all_in_one/basic_settings.sh
-
 if [ ! -d ${config_dir}/scripts/File_Permission_Changes_Plus ]; then
     mkdir -p ${config_dir}/scripts/File_Permission_Changes_Plus
     TEXT='创建脚本文件夹成功' && INFO
+else
+    TEXT='脚本文件夹已存在' && INFO
 fi
 if [ ! -f ${config_dir}/scripts/File_Permission_Changes_Plus/File_Permission_Changes_Plus.sh ]; then
-    curl -o ${config_dir}/scripts/File_Permission_Changes_Plus/File_Permission_Changes_Plus.sh https://ghproxy.com/
-    TEXT='下载File_Permission_Changes_Plus脚本成功'
-    INFO
+    curl -o \
+        ${config_dir}/scripts/File_Permission_Changes_Plus/File_Permission_Changes_Plus.sh \
+        https://ghproxy.com/https://github.com/DDS-Derek/VideoLab/raw/master/scripts/File_Permission_Changes_Plus/File_Permission_Changes_Plus.sh
+    if [ $? -eq 0 ]; then
+        TEXT='下载File_Permission_Changes_Plus脚本成功' && INFO
+    else
+        TEXT='下载File_Permission_Changes_Plus脚本失败' && ERROR
+        exit 1
+    fi
+else
+    TEXT='File_Permission_Changes_Plus以存在' && INFO
 fi
+
+get_cron(){
+echo -e "${Green}请输入Cron表达式（默认 */30 * * * * ）${Font}"
+read -ep "CRON:" NEW_CRON
+[[ -z "${NEW_CRON}" ]] && NEW_CRON="*/30 * * * *"
+}
+
+if [ ! -f ${config_dir}/scripts/File_Permission_Changes_Plus/cron.lock ]; then
+    touch ${config_dir}/scripts/File_Permission_Changes_Plus/cron.lock
+    (crontab -l ; echo "${NEW_CRON} /usr/bin/env bash ${config_dir}/scripts/File_Permission_Changes_Plus/File_Permission_Changes_Plus.sh") | crontab -
+    if [ $? -eq 0 ]; then
+        TEXT='定时任务设置成功' && INFO
+    else
+        TEXT='定时任务设置失败' && ERROR
+        exit 1
+    fi
+fi
+
+exit 0
